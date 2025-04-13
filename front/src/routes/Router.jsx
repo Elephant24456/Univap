a23ae8033e98e61ba6acffe49dd349439bc5fdcd
yshls <shluxnsal01@gmail.com>
2025-04-13 22:49:37 +0900

    feat: 글쓰기 페이지생성, 그리고 홈 화면에는 글쓰기로 가는 버튼 추가

--- front/src/routes/Router.jsx
+++ front/src/routes/Router.jsx
@@ -3,6 +3,7 @@ import LoginIntro from '../pages/LoginIntro';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 import Home from '../pages/Home';
+import Write from '../pages/Write';
 
 const Router = () => {
   return (
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/write" element={<Write />} />
       </Routes>
     </BrowserRouter>
   );
