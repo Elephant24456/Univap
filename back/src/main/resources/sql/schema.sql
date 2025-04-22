ea42410078fcc89d3d23b113ff9a2b7802afcbc8
unKILLableBRIAR <wnsgud3590@naver.com>
2025-04-23 00:50:09 +0900

    Feat: 초기 DB 설정 및 SQL 스크립트 추가
    -application.properties에 MySQL 접속 정보 설정
    -schema.sql: user 테이블 생성
    -data.sql: 테스트용 유저 데이터 삽입
    -test.sql: 테스트용 쿼리문 작성

---
+++ back/src/main/resources/sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE capstone_db CHARACTER SET utf8mb4; #db 생성
+
+USE capstone_db; #db 사용
+
+CREATE TABLE user( #user테이블 생성
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(50) NOT NULL,
+    univ VARCHAR(50) NOT NULL,
+    profile_img VARCHAR(255),
+    nickname VARCHAR(30) NOT NULL
+);
