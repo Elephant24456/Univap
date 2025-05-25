7c7b3b1095b8eb666d5729be0ef611ecb6241e62
unKILLableBRIAR <wnsgud3590@naver.com>
2025-05-25 18:28:45 +0900

    Feat: 게시물 테이블, 엔티티 생성

--- back/src/main/resources/sql/data.sql
+++ back/src/main/resources/sql/data.sql
@@ -8,3 +8,14 @@ VALUES(
        '/images/test.png',
        '해린냥이'
 );
+
+INSERT INTO post (title, date, time, location, content, user_id, nickname)
+VALUES(
+       '7시에 주먹고기가실분',
+       '2025-05-25',
+       '19:00:00',
+       '주먹고기',
+       '진짜 가서 밥만 먹죠, 우리가 할말은 주문뿐.',
+       '2',
+       '고슴도치'
+);
